30eecc8e55218f466ee8126996adadff494e79eb
jesperon <jesperon@continuumsecurity.net>
2020-04-24 14:27:13 +0200

    [feature/CON-426] Added a new feature to document IriusRisk Rules in HTML

---
+++ src/generateRulesGraph.py
@@ -0,0 +1,236 @@
+#!/usr/bin/python
+import sys
+import xml.etree.ElementTree as ET
+import networkx as nx
+import json
+import flask
+from networkx.readwrite import json_graph
+
+
+def getCondition(condition, componentDefinitions):
+    cname = condition.attrib['name']
+    group = 0
+    if cname == "Is specific component definition":
+        value = condition.attrib['value']
+        if value in componentDefinitions.keys():
+            message = cname + ": " + componentDefinitions[condition.attrib['value']]
+        else:
+            message = cname + ": " + value
+        group = 0
+    elif cname == "Question Group exists":
+        value = condition.attrib['value']
+        message = "If question group '" + value + "' exists"
+        group = 1
+    elif cname == "Question is answered":
+        value = condition.attrib['value']
+        message = "If answer is " + value
+        group = 2
+    elif cname == "Question is not answered":
+        value = condition.attrib['value']
+        message = "If answer is not " + value
+        group = 9
+    elif cname == "Risk pattern exists":
+        value = condition.attrib['value']
+        message = "Risk pattern " + condition.attrib['value'].split("_::_")[0] + " -> " + \
+                  condition.attrib['value'].split("_::_")[1] + " exists"
+        group = 3
+    elif cname == "Conclusion exists":
+        value = condition.attrib['value']
+        message = "Conclusion " + value + " exists"
+        group = 8
+    elif cname == "Conclusion not exists":
+        value = condition.attrib['value']
+        message = "Conclusion " + value + " not exists"
+        group = 12
+    elif cname == "Applied Countermeasure":
+        value = condition.attrib['value']
+        message = "Countermeasure " + value + " is required"
+        group = 13
+    else:
+        value = ""
+        message = "Unknown: "+cname
+    return value, message, group
+
+
+def getActions(action, components):
+    aname = action.attrib['name']
+    group = 0
+    if aname == "Insert Question Group":
+        valueList = action.attrib['value'].split("_::_")
+        value = valueList[0]
+        message = "Question: " + valueList[2]
+        group = 4
+    elif aname == "Insert Question":
+        valueList = action.attrib['value'].split("_::_")
+        value = valueList[0]
+        message = "Answer: " + valueList[1]
+        group = 5
+    elif aname == "Import Risk Pattern":
+        valueList = action.attrib['value'].split("_::_")
+        value = valueList[1]
+        message = "Import Risk Pattern: " + components[valueList[1]]
+        group = 6
+    elif aname == "Extend risk pattern":
+        valueList = action.attrib['value'].split("_::_")
+        value = action.attrib['value']
+        message = "Extend Risk Pattern " + valueList[0] + " >> " + valueList[1]
+        group = 7
+    elif aname == "Insert Conclusion":
+        valueList = action.attrib['value'].split("_::_")
+        value = valueList[1]
+        message = "Conclusion: " + valueList[2]
+        group = 10
+    elif aname == "Apply Control":
+        value = action.attrib['project'] + "_::_" + action.attrib['value']
+        message = "Apply Control: " + action.attrib['project'] + " -> " + action.attrib['value']
+        group = 11
+    else:
+        value = ""
+        message = "Unknown: "+str(aname)
+    return value, message, group
+
+
+def generateGraph(rules, G, components, componentDefinitions):
+    for rule in rules:
+        conditions = []
+        for condition in rule.iter('condition'):
+            value, message, group = getCondition(condition, componentDefinitions)
+            print(f'Adding node: "{value}":"{message}"')
+
+            if not G.has_node(value):
+                if message is "":
+                    message = value
+                G.add_node(value, message=message, group=group)
+            conditions.append(value)
+
+        for action in rule.iter('action'):
+            value, message, group = getActions(action, components)
+            if message is "":
+                message = value
+            G.add_node(value, message=message, group=group)
+
+            for condition in conditions:
+                # If value is null we have a problem :(
+                G.add_edge(condition, value)
+    return G
+
+
+def generateRulesGraph(xmlPath, **kwargs):
+    searchNode = kwargs.get('searchNode')
+    depth = kwargs.get('depth')
+    if depth:
+        depth = int(depth)
+    tree = ET.parse(xmlPath)
+    root = tree.getroot()
+
+    G = nx.DiGraph()
+    components = {}
+    componentDefinitions = {}
+
+    for component in root.find('components').iter('component'):
+        components[component.attrib['ref']] = component.attrib['name']
+
+    for component in root.find('componentDefinitions').iter('componentDefinition'):
+        componentDefinitions[component.attrib['ref']] = component.attrib['name']
+
+    # rules = []
+    # if searchNode and depth:
+    #     print(f"Searching {searchNode} with depth {depth}")
+    #     ids = []
+    #     searchPath = {searchNode}
+    #     for i in range(0, depth):
+    #         print("Level "+str(i))
+    #         print(searchPath)
+    #         for rule in root.find('rules').iter('rule'):
+    #             con = False
+    #             act = False
+    #             for condition in rule.iter('condition'):
+    #                 value, message, group = getCondition(condition, componentDefinitions)
+    #                 if value in searchPath and rule not in rules:
+    #                     rules.append(rule)
+    #                     con = True
+    #                 ids.append(value)
+    #             for action in rule.iter('action'):
+    #                 value, message, group = getActions(action, components)
+    #                 if value in searchPath and rule not in rules:
+    #                     rules.append(rule)
+    #                     act = True
+    #                 ids.append(value)
+    #             if con or act:
+    #                 for var in ids:
+    #                     searchPath.add(var)
+    #             else:
+    #                 ids = []
+    #
+    #
+    # else:
+    #     print("All nodes")
+    #     rules = list(root.find('rules').iter('rule'))
+    print("All nodes")
+    rules = list(root.find('rules').iter('rule'))
+    print(len(rules))
+
+    G = generateGraph(rules, G, components, componentDefinitions)
+
+    if searchNode is not "" and depth >= 0:
+        print("searching node " + searchNode)
+        ids = {searchNode}
+        newEdges = []
+        for i in range(0, depth):
+            for edge in G.edges:
+                source, target = edge
+                if source in ids:
+                    ids.add(target)
+                    newEdges.append(edge)
+                elif target in ids:
+                    ids.add(source)
+                    newEdges.append(edge)
+                else:
+                    pass
+
+        H = nx.DiGraph()
+        H.add_edges_from(newEdges)
+        for node in list(G.nodes.data()):
+            if node[0] in ids:
+                H.add_node(node[0], message=node[1]['message'], group=node[1]['group'])
+
+        d = json_graph.node_link_data(H)
+    else:
+        d = json_graph.node_link_data(G)
+    json.dump(d, open('../outFiles/graph/graph.json', 'w+'))
+
+    app = flask.Flask(__name__, static_folder="../outFiles/graph")
+    app.config["CACHE_TYPE"] = "null"
+
+    @app.route('/')
+    def static_proxy():
+        return app.send_static_file('index.html')
+
+    app.run(port=8000)
+
+
+def main():
+    searchNode = ""
+    depth = ""
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".xml"):
+        xmlPath = sys.argv[1]
+    elif len(sys.argv) == 4:
+        xmlPath = sys.argv[1]
+        searchNode = sys.argv[2]
+        depth = sys.argv[3]
+    else:
+        print("This script prints the relations between rules in a library. "
+              "Graph can be generated full (without parameters) "
+              "or scoped (defining node to watch and depth to backtrack)")
+        print("Graph will be served in a default Flask server (127.0.0.1:8000)")
+        print("Usage: python generateRulesGraph.py <library.xml> [<nodeId> <depth>]")
+        print("Examples: ")
+        print("python generateRulesGraph.py /path/to/CS-Default.xml")
+        print("python generateRulesGraph.py /path/to/CS-Default.xml CS-Default_::_DATASTORE 3")
+        print("python generateRulesGraph.py /path/to/CS-Default.xml bespokeCrypto 1")
+        return
+    generateRulesGraph(xmlPath, searchNode=searchNode, depth=depth)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/generateRulesHtml.py
@@ -0,0 +1,403 @@
+import os
+import sys
+from lxml import etree
+from pathlib import Path
+from src.generateChangeLogFromVersions import createTitleHtmlFile, writeToHtml
+import logging
+import re
+
+home = os.getcwd()
+logging.basicConfig(filename="logFile.log",
+                    format='%(asctime)s  %(levelname)-10s %(message)s',
+                    datefmt="%Y-%m-%d-%H-%M-%S",
+                    filemode='w')
+
+# Creating an object
+logger = logging.getLogger()
+
+# Setting the threshold of logger to INFO
+logger.setLevel(logging.INFO)
+
+questions = {
+    0: "Show all rules",
+    1: "Show all rules by library",
+    2: "For every library which rules use a specific answer?",
+    3: "From all the rules which ones import a risk pattern?",
+    4: "From all the rules which ones check if a risk pattern exists?",
+    5: "What possible ways we have available for a specific risk pattern to appear in the model?"
+}
+
+
+def createCardBodyCondition(condition, card, parent, num):
+    root = '#%s' % parent
+    carddivbody = etree.Element("div")
+    carddivbody.set('id', 'collapse' + str(num))
+    carddivbody.set('class', 'collapse')
+    carddivbody.set('aria-labelledby', 'heading' + str(num))
+    carddivbody.set('data-parent', root)
+
+    cardbody = etree.Element("div")
+    cardbody.set("class", "card-body")
+
+    parser = etree.HTMLParser()
+    b = etree.Element("b")
+    b.append(etree.fromstring("Attributes:", parser))
+    cardbody.append(b)
+    for name, value in condition.attrib.items():
+        cardbody.append(etree.fromstring(f'{name}="{value}"', parser))
+
+    carddivbody.append(cardbody)
+    card.append(carddivbody)
+    return card
+
+
+def createCardBodyAction(action, card, parent, num):
+    root = '#%s' % parent
+    carddivbody = etree.Element("div")
+    carddivbody.set('id', 'collapse' + str(num))
+    carddivbody.set('class', 'collapse')
+    carddivbody.set('aria-labelledby', 'heading' + str(num))
+    carddivbody.set('data-parent', root)
+
+    cardbody = etree.Element("div")
+    cardbody.set("class", "card-body")
+
+    parser = etree.HTMLParser()
+    b = etree.Element("b")
+    b.append(etree.fromstring("Attributes:", parser))
+    cardbody.append(b)
+    for name, value in action.attrib.items():
+        cardbody.append(etree.fromstring(f'{name}="{value}"', parser))
+
+    carddivbody.append(cardbody)
+    card.append(carddivbody)
+    return card
+
+
+def createCardHeaderConditionAction(title, num):
+    card = etree.Element("div")
+    cardheader = etree.Element("div")
+    cardheader.set('id', 'heading' + str(num))
+    h5 = etree.Element("h5")
+    h5.set("class", "mb-0")
+
+    button = etree.Element("button")
+
+    button.set('data-toggle', 'collapse')
+    button.set('data-target', '#collapse' + str(num))
+    button.set('aria-expanded', 'false')
+    button.set('aria-controls', 'collapse' + str(num))
+
+    card.set("class", "card border-success")
+    cardheader.set("class", "card-header")
+    button.set('class', 'btn btn-light btn-group-toggle')
+
+    h5.append(button)
+    cardheader.append(h5)
+    button.text = title
+    card.append(cardheader)
+    return card
+
+
+def createCardConditions(conditions, cardBody, num):
+    parent = "parent%i" % num
+    parentCard = etree.Element("div")
+    parentCard.set("class", "accordion")
+    parentCard.set("id", parent)
+    for condition in conditions:
+        num = num + 1
+        name = condition.attrib['name']
+        value = condition.attrib['value']
+        title = f"Condition: {name} [{value}]"
+        card = createCardHeaderConditionAction(title, num)
+        card = createCardBodyCondition(condition, card, parent, num)
+        parentCard.append(card)
+    cardBody.append(parentCard)
+    return cardBody, num
+
+
+def createCardActions(actions, cardBody, num):
+    parent = "parent%i" % num
+    parentCard = etree.Element("div")
+    parentCard.set("class", "accordion")
+    parentCard.set("id", parent)
+    for action in actions:
+        num = num + 1
+        name = action.attrib['name']
+        value = action.attrib['value']
+        title = f"Actions: {name} [{value}]"
+        card = createCardHeaderConditionAction(title, num)
+        card = createCardBodyAction(action, card, parent, num)
+        parentCard.append(card)
+    cardBody.append(parentCard)
+    return cardBody, num
+
+
+def createCardBody(card, rule, num):
+    carddivbody = etree.Element("div")
+    carddivbody.set('id', 'collapse' + str(num))
+    carddivbody.set('class', 'collapse')
+    carddivbody.set('aria-labelledby', 'heading' + str(num))
+    carddivbody.set('data-parent', '#accordion')
+
+    cardbody = etree.Element("div")
+    cardbody.set("class", "card-body")
+
+    conditions = list(rule.iter("condition"))
+    actions = list(rule.iter("action"))
+
+    cardbody, num = createCardConditions(conditions, cardbody, num)
+    cardbody, num = createCardActions(actions, cardbody, num)
+
+    carddivbody.append(cardbody)
+    card.append(carddivbody)
+    return card, num
+
+
+def createCardHeader(ruleName, num):
+    card = etree.Element("div")
+    cardheader = etree.Element("div")
+    cardheader.set('id', 'heading' + str(num))
+    h5 = etree.Element("h5")
+    h5.set("class", "mb-0")
+
+    button = etree.Element("button")
+
+    button.set('data-toggle', 'collapse')
+    button.set('data-target', '#collapse' + str(num))
+    button.set('aria-expanded', 'false')
+    button.set('aria-controls', 'collapse' + str(num))
+
+    card.set("class", "card border")
+    cardheader.set("class", "card-header")
+    button.set('class', 'btn btn-light btn-group-toggle')
+
+    h5.append(button)
+    cardheader.append(h5)
+    button.text = f"Rule: {ruleName}"
+    card.append(cardheader)
+    return card
+
+
+def createCardsForRules(rules, accordion, body):
+    num = 0
+
+    for rule in rules:
+        num = num + 1
+        ruleName = rule.attrib['name']
+        card = createCardHeader(ruleName, num)
+        card, num = createCardBody(card, rule, num)
+        accordion.append(card)
+        body.append(accordion)
+    return body
+
+
+def getRules(question, rules, checkAllRules, libraryName, value):
+    # Extract rules
+    titleParam = ""
+    if question == "From all the rules which ones import a risk pattern?":
+        response = []
+        for rule in rules:
+            for action in rule.iter('action'):
+                if action.attrib['name'] == "Import Risk Pattern":
+                    rp = action.attrib['value'].split("_::_")[1]
+                    if rp == value:
+                        response.append(rule)
+        rules = response
+        title = f"Rules from all libraries to answer question: {question}: {value}"
+        titleParam = value
+    elif question == "From all the rules which ones check if a risk pattern exists?":
+        response = []
+        for rule in rules:
+            for condition in rule.iter('condition'):
+                if condition.attrib['name'] == "Risk pattern exists":
+                    rp = condition.attrib['value'].split("_::_")[1]
+                    if rp == value:
+                        response.append(rule)
+        rules = response
+        title = f"Rules from all libraries to answer question: {question}: {value}"
+        titleParam = value
+    elif question == "Show all rules":
+        title = f"Rules from all libraries"
+    elif question == "For every library which rules use a specific answer?":
+        response = []
+        for rule in rules:
+            for condition in rule.iter('condition'):
+                if condition.attrib['name'] == "Question is answered":
+                    if condition.attrib['value'] == value:
+                        response.append(rule)
+        rules = response
+        title = f"Rules from the library {libraryName} that answer question: {question}:{value}"
+        titleParam = value
+    elif question == "Show all rules by library":
+        title = f"Rules from the library {libraryName}"
+    elif question == "What possible ways we have available for a specific risk pattern to appear in the model?":
+        response = []
+        for rule in rules:
+            for condition in rule.iter('condition'):
+                if condition.attrib['name'] == "Risk pattern exists":
+                    rp = condition.attrib['value'].split("_::_")[1]
+                    if rp == value:
+                        response.append(rule)
+            for action in rule.iter('action'):
+                if action.attrib['name'] == "Import Risk Pattern":
+                    rp = action.attrib['value'].split("_::_")[1]
+                    if rp == value:
+                        response.append(rule)
+        rules = response
+        title = f"Rules from all libraries to answer question: {question}: {value}"
+        titleParam = value
+    else:
+        rules = []
+        title = f"Wrong question {question}"
+    return rules, title, titleParam
+
+
+def createCollapseExpandButtons(body, root):
+    collapse = etree.Element("button")
+    collapse.text = "Collapse All"
+    collapse.set("id", "collapse")
+
+    expand = etree.Element("button")
+    expand.text = "Expand All"
+    expand.set("id", "expand")
+
+    div = etree.Element("div")
+    div.append(collapse)
+    div.append(expand)
+    div.set("style","justify-content: center;text-align: center;font-weight: bold;")
+
+    body.append(div)
+
+    script = etree.Element("script")
+    script.text = '$("#collapse").click(function(){$("div[id^=\'collapse\']").removeClass("show");});$("#expand").click(function(){$("div[id^=\'collapse\']").addClass("show");});'
+    root.append(script)
+
+    return body
+
+
+def createCardComponent(componentName, searchValue, num):
+    card = etree.Element("div")
+    cardheader = etree.Element("div")
+    cardheader.set('id', 'headingComp' + str(num))
+    h5 = etree.Element("h5")
+    h5.set("class", "mb-0")
+
+    button = etree.Element("button")
+
+    button.set('data-toggle', 'collapse')
+    button.set('data-target', '#collapseComp' + str(num))
+    button.set('aria-expanded', 'false')
+    button.set('aria-controls', 'collapseComp' + str(num))
+
+    card.set("class", "card border")
+    cardheader.set("class", "card-header")
+    button.set('class', 'btn btn-light btn-group-toggle')
+
+    h5.append(button)
+    cardheader.append(h5)
+    button.text = f"Component: {componentName} imports risk pattern {searchValue}"
+    card.append(cardheader)
+
+    return card
+
+
+def createCardsForComponents(searchValue, components, accordion, body):
+    num = 0
+    if searchValue:
+        for component in components:
+            if searchValue in [i.attrib['ref'] for i in list(component.iter('riskPattern'))]:
+                num = num + 1
+                componentName = component.attrib['name']
+                card = createCardComponent(componentName, searchValue, num)
+                accordion.append(card)
+                body.append(accordion)
+    return body
+
+
+def getComponents(question, searchValue, components):
+    filteredComponents = []
+    if searchValue and question in [questions[3], questions[5]]:
+        for component in components:
+            if searchValue in [i.attrib['ref'] for i in list(component.iter('riskPattern'))]:
+                filteredComponents.append(component)
+    return filteredComponents
+
+def createBody(question, rules, components, body, root, checkAllRules, h1, value):
+    rules, title, titleParam = getRules(question, rules, checkAllRules, h1, value)
+    components = getComponents(question, value, components)
+    title = title + " [ " + str(len(components)) + " components ][ " + str(len(rules)) + " rules ]"
+    accordion = createTitleHtmlFile(title=title, body=body)
+    body = createCollapseExpandButtons(body, root)
+    createCardsForComponents(value, components, accordion, body)
+    createCardsForRules(rules, accordion, body)
+    titleQuestion = capitalizeFileName(question)
+    return body, title, titleQuestion, titleParam
+
+
+def generateRulesHtml(arrayLibrariesPaths, question, checkAllRules, value):
+
+    titleParam = "NoParameters"
+    base_html_path = Path.cwd() / "src" / "resources" / "changelog_base_html.html"
+    parser = etree.HTMLParser()
+    links = []
+
+    # Returns answer to the questions for all rules in one single file
+    if checkAllRules:
+        file = etree.parse(str(base_html_path), parser)
+        root = file.getroot()
+        body = root.find("body")
+
+        rules = []
+        components = []
+        for libraryPath in arrayLibrariesPaths:
+            print("Checking "+str(libraryPath))
+            treeroot = etree.parse(str(libraryPath)).getroot()
+            rules = rules + list(treeroot.find('rules').iter('rule'))
+            components = components + list(treeroot.find('componentDefinitions'))
+
+        body, title, titleQuestion, titleParam = createBody(question, rules, components, body, root, checkAllRules, "All libraries", value)
+
+        outFile_path = Path.cwd() / "outFiles" / "generatedHtml" / f"rules_{titleQuestion}_{capitalizeFileName(titleParam)}_AllLibraries.html"
+        root.find("head").find("title").text = title
+        writeToHtml(outFile_path, file.getroot())
+        links.append(outFile_path)
+    else:
+        # Returns answer to the questions separated by libraries
+        for libraryPath in arrayLibrariesPaths:
+            file = etree.parse(str(base_html_path), parser)
+            root = file.getroot()
+            body = root.find("body")
+            libraryName = str(os.path.splitext(os.path.basename(libraryPath))[0])
+            treeroot = etree.parse(str(libraryPath)).getroot()
+
+            components = list(treeroot.find('componentDefinitions'))
+            rules = list(treeroot.find('rules').iter('rule'))
+
+            body, title, titleQuestion, titleParam = createBody(question, rules, components, body, root, checkAllRules, libraryName, value)
+
+            outFile_path = Path.cwd() / "outFiles" / "generatedHtml" / f"rules_{titleQuestion}_{capitalizeFileName(titleParam)}_{libraryName}.html"
+            root.find("head").find("title").text = title
+            writeToHtml(outFile_path, file.getroot())
+            links.append(outFile_path)
+
+    return links
+
+
+def capitalizeFileName(filename):
+    return re.sub(r'\W+', '', ''.join(x.capitalize() for x in filename.split(" ")))
+
+
+# Usage:
+# ToolKit: select libraries, question and parameters for that question in the GUI.
+# Results are exported in outFiles/generatedHtml/rules_*.html
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        riskPattern = "DATASTORE"
+        questionAnswered = "aws.sns.yes"
+        question = questions[5]
+        # TODO: Do this with opts
+        generateRulesHtml(arrayLibrariesPaths=sys.argv[1:],
+                          question=question,
+                          checkAllRules=True,
+                          value=riskPattern)
